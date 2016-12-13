40abe54e483ed39909bfe6e8eeaf3dcaa760a1cf
Jack Kaloger <r5.defour@gmail.com>
2016-12-14 03:13:38 +1100

    Added task creation
    Form allows user to add a task

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,3 +9,24 @@ Template.body.helpers({
 		return Tasks.find({});
 	},
 });
+
+Template.body.events({
+	'submit .new-task'(event) {
+		// prevents browser from submitting
+		event.preventDefault();
+
+		// grab value from input
+		const target = event.target;
+		const text = target.text.value;
+
+		// now insert the task into the collection
+		Tasks.insert({
+			text,
+			// set as current time
+			createdAt: new Date(),
+		});
+
+		// clear
+		target.text.value = '';
+	}
+})
